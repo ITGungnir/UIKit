967e7c09306cfe831d0532ccebf95e1f0812b975
ITGungnir <itgungnir@163.com>
2020-01-19 17:32:06 +0800

    完成三个控件的demo

--- app/src/main/java/test/itgungnir/ui/status_view/StatusViewActivity.kt
+++ app/src/main/java/test/itgungnir/ui/status_view/StatusViewActivity.kt
@@ -2,6 +2,8 @@ package test.itgungnir.ui.status_view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_status_view.*
+import my.itgungnir.ui.status_view.StatusView
 import test.itgungnir.ui.R
 
 /**
@@ -14,5 +16,32 @@ class StatusViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_status_view)
+
+        initView()
+    }
+
+    private fun initView() {
+        // StatusView
+        status_view
+            .addDelegate(StatusView.Status.LOADING, R.layout.view_status_loading) {}
+            .addDelegate(StatusView.Status.SUCCEED, R.layout.view_status_succeed) {}
+            .addDelegate(StatusView.Status.EMPTY, R.layout.view_status_empty) {}
+            .addDelegate(StatusView.Status.FAILED, R.layout.view_status_failed) {}
+        // StatusToggle - Loading
+        status_loading.setOnClickListener {
+            status_view.loading { }
+        }
+        // StatusToggle - Succeed
+        status_success.setOnClickListener {
+            status_view.succeed { }
+        }
+        // StatusToggle - Empty
+        status_empty.setOnClickListener {
+            status_view.empty { }
+        }
+        // StatusToggle - Failed
+        status_fail.setOnClickListener {
+            status_view.failed { }
+        }
     }
 }
